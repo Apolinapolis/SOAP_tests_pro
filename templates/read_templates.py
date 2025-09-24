4cec5fd35e3eb22a32fab8ae90ad9b2ba62097a2
Apolinapolis <larra12@ya.ru>
2025-09-24 21:18:52 +0300

    dinamic created temp.xsd

--- templates/read_templates.py
+++ templates/read_templates.py
@@ -1,3 +1,5 @@
+import xmlschema
+from xmlschema import XMLSchema11
 from jinja2 import Environment, select_autoescape, FileSystemLoader
 
 
@@ -13,3 +15,10 @@ def iso_code_xml_creator(country_iso_code:str) -> str:
     return template.render({'iso_code':country_iso_code})
 
 
+def xsd_response_creator(operation:str) -> XMLSchema11:
+    envelop_xsd = env.get_template('./templates/xsd/envelope.xsd')
+    country_iso_code_response = envelop_xsd.render(
+        {'operation_xsd': f'{operation}.xsd', 'operation': operation})
+    with open ('./templates/xsd/temp.xsd', 'w') as file:
+        file.write(country_iso_code_response)
+    return  xmlschema.XMLSchema11('./templates/xsd/temp.xsd')
--- tests/soap/tests_soap.py
+++ tests/soap/tests_soap.py
@@ -1,12 +1,14 @@
 import allure
 
-from templates.read_templates import iso_code_xml_creator
+from templates.read_templates import iso_code_xml_creator, xsd_response_creator
 
 
 @allure.feature('SOAP')
 class TestSoap:
 
     def test_country_name(self, soap_session):
-        response = soap_session.request(method='POST', data=iso_code_xml_creator('RU'))
+        response = soap_session.request(method='POST', data=iso_code_xml_creator('IT'))
         assert iso_code_xml_creator
         assert response.status_code == 200
+
+        xsd_response_creator('CountryNameResponse').validate(response.text)
