c0d6b126bf122fafc3acf2968fab522e96923702
Apolinapolis <larra12@ya.ru>
2025-09-28 14:51:50 +0300

    refactoring

--- templates/utils.py
+++ templates/utils.py
@@ -2,6 +2,8 @@ import xml.etree.ElementTree as ET
 
 
 
-def check_value_result(expected_value:str, response_value:str) -> bool:
-    root = ET.fromstring(response_value)
-    return root[0][0][0].text == expected_value
+def check_value_result(expected: str, response_value: str) -> bool:
+    try:
+        return ET.fromstring(response_value)[0][0][0].text == expected
+    except (IndexError, AttributeError):
+        return False
